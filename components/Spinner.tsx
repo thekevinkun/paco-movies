1fab973ccce6a02f5fbc0d53c0c05fcdae1b3a5c
thekevinkun <developer.kevinkun@gmail.com>
2025-05-09 00:25:25 +0800

    Revert back to old loading screen

--- components/Spinner.tsx
+++ components/Spinner.tsx
@@ -3,15 +3,13 @@ import Image from "next/image";
 export default function Spinner() {
   return (
     <div className="h-[92vh] flex flex-col items-center justify-center">
-      <Image 
-        src="/icons/paco-movies-icon.png"
-        alt="PacoMovies icon"
-        width={32}
-        height={32}
-        sizes="32px"
-        className="aspect-square object-cover rounded-full"
+      <Image
+        src="/icons/spinner.svg"
+        alt="Loading Spinner"
+        width={75}
+        height={75}
+        className="object-contain"
       />
-      <h2 className="pt-4 font-medium text-xl text-main">Loading PacoMovies...</h2>
     </div>
   );
 }
