90b14c19c6c751d9e813accdee7181cb421b7585
thekevinkun <developer.kevinkun@gmail.com>
2025-05-09 00:07:46 +0800

    Try to capture better by changing loading screen

--- components/Spinner.tsx
+++ components/Spinner.tsx
@@ -3,13 +3,15 @@ import Image from "next/image";
 export default function Spinner() {
   return (
     <div className="h-[92vh] flex flex-col items-center justify-center">
-      <Image
-        src="/icons/spinner.svg"
-        alt="Loading Spinner"
-        width={75}
-        height={75}
-        className="object-contain"
+      <Image 
+        src="/icons/paco-movies-icon.png"
+        alt="PacoMovies icon"
+        width={32}
+        height={32}
+        sizes="32px"
+        className="aspect-square object-cover rounded-full"
       />
+      <h2 className="pt-4 font-medium text-xl text-main">Loading PacoMovies...</h2>
     </div>
   );
 }
